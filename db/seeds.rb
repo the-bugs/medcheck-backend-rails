98430a03f46bc2f77271f5f783f68568debc9437
Matheus Victor <matheusvictor.salles@gmail.com>
2023-11-08 12:27:35 -0300

    feat: Add migrations e seeds de Especialidade

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Especialidade.create(nome: "Cardiologia")
+Especialidade.create(nome: "Ortopedia")
+Especialidade.create(nome: "Terapeuta")
+Especialidade.create(nome: "Dermatologia")
+
+puts "Especialidades populadas com sucesso!"
