90b1b2facf66eb25a77ddaa52c05acb9bea6dbc1
Rafael Coelho <rafallcoelho@gmail.com>
2023-11-21 18:46:50 -0300

    change tabs to spaces (two)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,29 +2,29 @@ class User < ApplicationRecord
   has_secure_password
   after_initialize :default_values
 
-	validates :email, presence: true, uniqueness: true
-	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-	# validates :password,
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  # validates :password,
   #           # length: { minimum: 8 },
   #           # if: -> { new_record? || !password.nil? }
   
-	has_one :patient
-	has_one :medic
-	has_one :record, through: :patient
-	has_one :specialty, through: :medic
+  has_one :patient
+  has_one :medic
+  has_one :record, through: :patient
+  has_one :specialty, through: :medic
 
-	enum user_type: {
-		Medico: 1,
-		Paciente: 2,
-	}
+  enum user_type: {
+    Medico: 1,
+    Paciente: 2,
+  }
 
-	def admin?
-		self.is_admin
-	end
+  def admin?
+    self.is_admin
+  end
 
-	private
+  private
 
-	def default_values
-		self.is_admin |= false
-	end
+  def default_values
+    self.is_admin |= false
+  end
 end
