d766cd78b5373ef574572347ecf1752bc2095ed5
Matheus Victor <matheusvictor.salles@gmail.com>
2023-11-23 18:43:45 -0300

    feat: Add listagem de Médicos por Especialidade

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :records, path: :prontuarios
   resources :patients, path: :pacientes
   resources :users, path: :usuarios
+
+  get '/medicos/especialidades/:id', to: 'medics#medics_by_specialty'
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
