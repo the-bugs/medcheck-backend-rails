d2c0123d2b4d5f42af47e84b2e58809563a0392d
Matheus Victor <matheusvictor.salles@gmail.com>
2023-11-23 18:54:23 -0300

    feat: Add listagem Agendas por Médico

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users, path: :usuarios
 
   get '/medicos/especialidades/:id', to: 'medics#medics_by_specialty'
+  get '/agendas/medicos/:id', to: 'schedules#schedules_by_medics'
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
